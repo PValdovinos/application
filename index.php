95cd5f2615b9e81918c14533fe5e845442e754a4
Pedro V.R. <ptreyes2@gmail.com>
2024-04-19 20:37:21 -0400

    Added buttons and personal info section, have to see why 403 error is popping up still

--- index.php
+++ index.php
@@ -4,34 +4,59 @@
 
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
-// 328/diner/index.php
-// This is my CONTROLLER
 
-// Turn on error reporting
+// Include the Fat-Free Framework
 require_once ('vendor/autoload.php');
 
 // Instantiate base class
 $f3 = Base::instance();
 
-//define a default route
-//$f3->route('GET /', function(){
-//
-//    //this echo is for pets, but this index is for application, use a necessary
-//    //echo '<h1>Pet Home</h1>';
-//
-//    //render a view page
-//    $view = new Template();
-//    echo $view->render('views/home.html');
-//});
+// Define a default route
+$f3->route('GET /', function($f3){
 
-// Define order route
+    // Render the home page
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+// Define apply route
 $f3->route('GET|POST /apply', function($f3){
 
-    echo '<h1>Personal Information Page</h1>';
+    // If form is submitted
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        // Retrieve form data
+        $firstName = $_POST['firstName'];
+        $lastName = $_POST['lastName'];
+        $email = $_POST['email'];
+        $state = $_POST['state'];
+        $phone = $_POST['phone'];
 
-    //render a view page
+        // Store data in F3 hive for access in template
+        $f3->set('firstName', $firstName);
+        $f3->set('lastName', $lastName);
+        $f3->set('email', $email);
+        $f3->set('state', $state);
+        $f3->set('phone', $phone);
+
+        // Redirect to confirmation page
+        $f3->reroute('/confirmation');
+    } else {
+        // If form is not submitted, render personal information page
+        echo '<h1>Personal Information Page</h1>';
+        // Render a view page
+        $view = new Template();
+        echo $view->render('views/personal-information.html');
+    }
+});
+
+// Define confirmation route
+$f3->route('GET /confirmation', function($f3){
+
+    // Render confirmation page
+    echo '<h1>Confirmation Page</h1>';
+    // Render a view page
     $view = new Template();
-    echo $view->render('views/personal-information.html');
+    echo $view->render('views/confirmation.html');
 });
 
 // Run Fat-Free
